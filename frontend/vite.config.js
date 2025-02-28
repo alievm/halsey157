f3229725169e829f7a8268c1ce05789357b75998
root <root@srv737057.hstgr.cloud>
2025-02-28 05:11:46 +0000

    test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+     host: 'halsey157news.com',
+     strictPort: false,
+     allowedHosts: true,
+     hmr: {
+        protocol: 'ws',
+        host: 'localhost'
+     }
+    }
 })
